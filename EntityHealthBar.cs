e0c0720f4cc69d7226f86658aebd73084c1537a3
ElliotRedhead <elliot.redhead@gmail.com>
2021-10-16 19:32:27 +0100

    Git init

---
+++ EntityHealthBar.cs
@@ -0,0 +1,19 @@
+﻿namespace Oxide.Plugins
+{
+	[Info("EntityHealthBar", "ExpertRaptor", "0.0.1")]
+	[Description("Display health bar for attacked entities.")]
+	class EntityHealthBar : RustPlugin
+	{
+		void OnEntityTakeDamage(BaseCombatEntity victimEntity, HitInfo hitInfo)
+		{
+			if (victimEntity is BasePlayer)
+				return;
+			if (hitInfo?.InitiatorPlayer is BasePlayer)
+			{
+				Puts(victimEntity.startHealth.ToString());
+				Puts(victimEntity._health.ToString());
+				Puts(victimEntity.healthFraction.ToString());
+			}
+		}
+	}
+}
