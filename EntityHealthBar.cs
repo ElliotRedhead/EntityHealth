d35656a32c8fa95ba139f5be0037072e7a9c9769
ElliotRedhead <elliot.redhead@gmail.com>
2021-10-18 22:25:46 +0100

    Show dmg & range of attack on NPC or other players

--- EntityHealthBar.cs
+++ EntityHealthBar.cs
@@ -4,15 +4,28 @@
 	[Description("Display health bar for attacked entities.")]
 	class EntityHealthBar : RustPlugin
 	{
+		void NotifyEntityStateInChat(BasePlayer recipient, BaseCombatEntity victimEntity, int damage, float distance)
+		{
+			int victimMaxHealth = (int)victimEntity.startHealth;
+			int victimPreviousHealth = (int)victimEntity._health;
+			int victimResultingHealth = victimPreviousHealth - damage;
+			string distanceString = distance.ToString("0.00");
+
+			recipient.ChatMessage($"{victimResultingHealth.ToString()}/{victimMaxHealth.ToString()}, {distanceString}");
+		}
 		void OnEntityTakeDamage(BaseCombatEntity victimEntity, HitInfo hitInfo)
 		{
-			if (victimEntity is BasePlayer)
+			if (victimEntity is BasePlayer && !(hitInfo.Initiator is BasePlayer))
 				return;
-			if (hitInfo?.InitiatorPlayer is BasePlayer)
+
+			var attacker = hitInfo.InitiatorPlayer;
+			if (hitInfo != null && attacker is BasePlayer)
 			{
-				Puts(victimEntity.startHealth.ToString());
-				Puts(victimEntity._health.ToString());
-				Puts(victimEntity.healthFraction.ToString());
+				float damageFloat = hitInfo.damageTypes.Get(hitInfo.damageTypes.GetMajorityDamageType());
+				int damage = (int)hitInfo.damageTypes.Total();
+				float distance = hitInfo.ProjectileDistance;
+				NotifyEntityStateInChat(attacker, victimEntity, damage, distance);
+
 			}
 		}
 	}
